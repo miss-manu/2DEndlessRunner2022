cf91aa6131b0d1272cb2a22bc8c50f90c0bce9ec
nagarajanm@wsc.school.nz <>
2022-03-03 11:22:26 +1300

    Added offscreen destroyers and prefab tags

---
+++ Assets/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOffScreen : MonoBehaviour
+{
+    // 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the collision was triggered by Player
+        if (collision.CompareTag("Player"))
+        {
+            // Then pause the game & return to editor
+            Debug.Break();
+            return;
+        }
+        else if (collision.CompareTag("Prefab"))
+        {
+            Debug.Log("Collision was caused by: " + collision.tag);     // Check a prefab caused the collision
+            Destroy(collision.gameObject);                              // Then destroy the prefab
+        }
+    }
+}
